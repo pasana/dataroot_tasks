002e372e41c7f4733ed3e05019934f6bf7a9ef84
pasana <milana_led@mail.ru>
2016-09-22 21:51:11 +0300

    bookimed_events

--- bookimed/site_position/site_position/items.py
+++ bookimed/site_position/site_position/items.py
@@ -11,5 +11,12 @@ import scrapy
 class SitePositionItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    clinic_id = scrapy.Field()
     name = scrapy.Field()
+    date = scrapy.Field()
+    full_rate = scrapy.Field()
+    rate_num = scrapy.Field()
+    good = scrapy.Field()
+    bad = scrapy.Field()
+    review = scrapy.Field()
     pass
